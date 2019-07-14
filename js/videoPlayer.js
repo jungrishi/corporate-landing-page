4d262f8d0fd25abaa27ad675e61f4bdcb98e1c05
jungrishi <ranabhat.85@gmail.com>
2019-07-14 07:49:24 +0545

    compltere

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -2,19 +2,38 @@
     var playbutton = document.getElementById("playButton");
     var video = document.getElementById("video");
     var startContent = document.getElementById("startContent");
+    var headline = document.getElementById("headline");
 
-    playbutton.addEventListener("click", () => {
-        if (video.play) {
+
+    // playbutton.addEventListener("click", () => {
+    //     if (video.onplay) {
+    //         startContent.style.display = "none";
+    //         video.style.display = "block";
+    //         playbutton.style.display = "block";
+    //         // playbutton.style.
+    //         video.pause();
+    //     }
+    //     else {
+    //         startContent.style.display = "block";
+    //         video.style.display = "none";
+    //         video.play();
+    //     }
+    // })
+
+    headline.addEventListener("click", () => {
+        if (video.onplay) {
             startContent.style.display = "none";
             video.style.display = "block";
             playbutton.style.display = "block";
             // playbutton.style.
             video.pause();
         }
-        else {
-            startContent.style.display = "block";
+        else if (video.onpause) {
             video.style.display = "none";
-            video.play();
+            startContent.style.display = "block";
         }
     })
+
+    console.log('heueue')
+
 })();
