780388fbccd3c32d8724349e8ac5545e921f5afe
jungrishi <ranabhat.85@gmail.com>
2019-07-15 10:12:19 +0545

    final version of landing page

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,39 +1,22 @@
 (function() {
-    var playbutton = document.getElementById("playButton");
+    var play = document.getElementById("playButton");
     var video = document.getElementById("video");
     var startContent = document.getElementById("startContent");
     var headline = document.getElementById("headline");
+    var h = document.getElementById("ba");
+    var playButton = play.childNodes[1];
 
+    headline.addEventListener("click",() => {
+        if (video.paused) {
+        headline.style.cursor = "pointer";
+        startContent.style.display = 'none';
+        video.play();
 
-    // playbutton.addEventListener("click", () => {
-    //     if (video.onplay) {
-    //         startContent.style.display = "none";
-    //         video.style.display = "block";
-    //         playbutton.style.display = "block";
-    //         // playbutton.style.
-    //         video.pause();
-    //     }
-    //     else {
-    //         startContent.style.display = "block";
-    //         video.style.display = "none";
-    //         video.play();
-    //     }
-    // })
-
-    headline.addEventListener("click", () => {
-        if (video.onplay) {
-            startContent.style.display = "none";
-            video.style.display = "block";
-            playbutton.style.display = "block";
-            // playbutton.style.
+    }
+        else {
+            startContent.style.display = 'block';
+            video.load();
             video.pause();
         }
-        else if (video.onpause) {
-            video.style.display = "none";
-            startContent.style.display = "block";
-        }
     })
-
-    console.log('heueue')
-
 })();
